086ce65750360b37cd9122228a7b29bae2348b0d
smitesh07 <smitesh.modak@colorado.edu>
2018-10-01 01:47:17 -0600

    Added Average and Plot Features

--- Project 1/My_Application/my_application.py
+++ Project 1/My_Application/my_application.py
@@ -4,6 +4,7 @@ import time
 from PyQt5.QtWidgets import QWidget,QDialog,QApplication,QMessageBox,QInputDialog,QLineEdit
 from PyQt5.QtCore import QTimer,QTime,QThread,QEventLoop
 from Project1 import Ui_Form
+import matplotlib.pyplot as plt
 
 ##class timerThread(QThread):
 ##    def collectData(self):
@@ -23,10 +24,17 @@ class AppWindow(QDialog):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         
-        global threshold,deg,fah
-        deg = 0
+        global threshold,deg,fah,temp_count,hum_count,temp_avg,hum_avg
+        global temp_samples,hum_samples
+        temp_samples=[]
+        hum_samples=[]
+        deg = 1
         fah = 0
         threshold = 35
+        temp_count=0
+        hum_count=0
+        temp_avg=0
+        hum_avg=0
         self.show()
 ##        self.initTimer()
         self.check()
@@ -38,21 +46,27 @@ class AppWindow(QDialog):
         self.ui.degree_button.clicked.connect(self.degTemp)
         self.ui.fahrenheit_button.clicked.connect(self.fahTemp)
         self.ui.refresh.clicked.connect(self.startTimer)
+        self.ui.stop_refresh.clicked.connect(self.stopTimer)
+        self.ui.plotGraph.clicked.connect(self.plotGraphs)
         self.ui.exit.clicked.connect(self.close)
 
     def startTimer(self):
         self.timer=QTimer()
         self.timer.timeout.connect(self.getTempHum)
         self.timer.start(1000)
+        self.ui.refresh_checkBox.setChecked(True)
 ##    def start_timer(self):
 ##        self.timer = timerThread()
 ##        self.timer.start()
-        
+    
+    def stopTimer(self):
+        self.timer.stop()
+        self.ui.refresh_checkBox.setChecked(False)
   
     def check(self):
-        hum, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
 
-        if hum is None or temperature is None:
+        if humidity is None or temperature is None:
             alert=QMessageBox()
             alert.setIcon(QMessageBox.Critical)
             alert.setText("Sensor not detected!!!!!")
@@ -68,22 +82,30 @@ class AppWindow(QDialog):
 ##            alert.exec_()            
         
     def getTemp(self):
-        global threshold,deg,fah
+        global threshold,deg,fah,temp_count,temp_avg
+        global temp_samples,temp_count
         self.check()
-        hum, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
         time=QTime.currentTime().toString()
         self.ui.temp_TOR.display(time)
         
-        if hum is None or temperature is None:
+        if humidity is None or temperature is None:
             alert=QtGui.QMessageBox()
             alert.setText("Sensor not detected!!!!!")
             alert.exec_()
             self.ui.temp.display("")
         else:
+            
             if deg == 1:
                 temp = '{0:.2f}'.format(temperature)
                 self.ui.temp.display(temp)
+                temp_samples.append(temperature)
                 deg = 0
+                temp_count += 1
+##                print(temp_count)
+                temp_avg=temp_avg+(temperature-temp_avg)/temp_count
+                temp_avg_display = '{0:.2f}'.format(temp_avg)
+                self.ui.tempAvg.display(temp_avg_display)
             elif fah == 1:
                 temperature = temperature*1.8 + 32
                 temp = '{0:.2f}'.format(temperature)
@@ -92,50 +114,79 @@ class AppWindow(QDialog):
             else:
                 temp = '{0:.2f}'.format(temperature)
                 self.ui.temp.display(temp)
+                
+            if threshold is None:
+                pass
+            elif temperature > threshold:
+                alert=QMessageBox()
+                alert.setIcon(QMessageBox.Warning)
+                alert.setText("High Temperature!!!!!")
+                alert.exec_()
             
     def getHum(self):
+        global hum_samples,hum_count,hum_avg
         self.check()
-        hum, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
         time=QTime.currentTime().toString()
         self.ui.hum_TOR.display(time)
         
-        if hum is None or temperature is None:
+        if humidity is None or temperature is None:
             alert=QtGui.QMessageBox()
             alert.setText("Sensor not detected!!!!!")
             alert.exec_()
             self.ui.hum.display("")
         else:
-            hum = '{0:.2f}'.format(hum)
+            hum = '{0:.2f}'.format(humidity)
             self.ui.hum.display(hum)
+            hum_samples.append(humidity)
+            hum_count +=1
+##            print(hum_count)
+            hum_avg=hum_avg+(humidity-hum_avg)/hum_count
+            hum_avg_display = '{0:.2f}'.format(hum_avg)
+            self.ui.humAvg.display(hum_avg_display)
         
     def getTempHum(self):
-        global threshold
+        global threshold,temp_count,temp_avg,hum_count,hum_avg
+        global hum_samples, temp_samples
         self.check()
-        hum, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
         time=QTime.currentTime().toString()
         self.ui.temp_TOR.display(time)
         self.ui.hum_TOR.display(time)
         
-        if hum is None or temperature is None:
+        if humidity is None or temperature is None:
             alert=QtGui.QMessageBox()
             alert.setText("Sensor not detected!!!!!")
             alert.exec_()
             self.ui.temp.display("")
             self.ui.hum.display("")
         else:
-            hum = '{0:.2f}'.format(hum)
+            hum = '{0:.2f}'.format(humidity)
             self.ui.hum.display(hum)
+            hum_samples.append(humidity)
             temp = '{0:.2f}'.format(temperature)
             self.ui.temp.display(temp)
+            temp_samples.append(temperature)
+            temp_count += 1
+            hum_count +=1
+##            print(temp_count)
+##            print(hum_count)
+            temp_avg=temp_avg+(temperature-temp_avg)/temp_count
+            hum_avg=hum_avg+(humidity-hum_avg)/hum_count
+            hum_avg_display = '{0:.2f}'.format(hum_avg)
+            self.ui.humAvg.display(hum_avg_display)
+            temp_avg_display = '{0:.2f}'.format(temp_avg)
+            self.ui.tempAvg.display(temp_avg_display)
+##            print(type(temp))
             #Set an alert for high temperautre
             if threshold is None:
                 pass
             elif temperature > threshold:
-                alert=QtGui.QMessageBox()
+                alert=QMessageBox()
+                alert.setIcon(QMessageBox.Warning)
                 alert.setText("High Temperature!!!!!")
                 alert.exec_()
-            else:
-                pass
+        
 
 ##        newtime = time.strftime('%m-%d-%y  %H:%M:%S')
         #self.ui.timeDisplay.setText(newtime)
@@ -154,6 +205,24 @@ class AppWindow(QDialog):
         global fah
         fah = 1
         self.getTemp()
+    
+    def plotGraphs(self):
+        global temp_count,hum_count,temp_samples,hum_samples
+        x_temp = [(i+1) for i in range(temp_count)]
+        x_hum = [(i+1) for i in range(hum_count)]
+        plt.subplot(211)
+        plt.plot(x_temp, temp_samples)
+        plt.xlabel('Sample Number')
+        plt.ylabel('Degree Celcius')
+        plt.title('Temperature Graph')
+        plt.subplot(212)
+        plt.plot(x_hum, hum_samples)
+        plt.xlabel('Sample Number')
+        plt.ylabel('% Humidity')
+        plt.title('Humidity Graph')
+        plt.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace=1.0)
+        plt.legend()
+        plt.show()
             
     def close(self):
         sys.exit(app.exec_())
